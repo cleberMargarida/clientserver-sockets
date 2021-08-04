6cbe25e82a67494fea2a4b627ca0643180aca174
cleberMargarida <cleberfilho3@gmail.com>
2021-08-04 19:20:17 -0300

    Dependency Injection
    Its make dependency injection pattern by Locator class.

---
+++ Server/ServiceLocator.cs
@@ -0,0 +1,42 @@
+﻿using Logic.Assignatures;
+using Logic.Implemenations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    /// <summary>
+    /// Class to provides many instances of services.
+    /// </summary>
+    using Locator = Dictionary<Object, Object>;
+    public static class ServiceLocator
+    {
+        private static Locator services = new Locator();
+
+        private static void InitServices() =>
+            services.AddScoped<IMaioridade, Maioridade>();
+
+        public static T UseService<T>()
+        {
+            if (services.IsNotInitiated())
+            {
+                InitServices();
+            }
+
+            return (T)services[typeof(T)];
+        }
+
+        private static Locator AddScoped<T, TT>(this Locator services)
+        {
+            services
+                .Add(typeof(T), (T)Activator.CreateInstance(typeof(TT)));
+            
+            return services;
+        }
+
+        private static bool IsNotInitiated(this Locator services) => services.Count == 0;
+    }
+}
